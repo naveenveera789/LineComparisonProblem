727d7674c9271233535f2098295611aa36d9629b
naveenveera789 <naveenveera789@gmail.com>
2021-11-04 11:57:24 +0530

    [naveenveera789] add : Line comparison computation is created

---
+++ LineComparison/LineComparison/LineCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public class LineCalculation
+    {
+        double xOnePoint, xTwoPoint, yOnePoint, yTwoPoint;
+        public LineCalculation(double xOnePoint, double xTwoPoint, double yOnePoint, double yTwoPoint)
+        {
+            this.xOnePoint = xOnePoint;
+            this.xTwoPoint = xTwoPoint;
+            this.yOnePoint = yOnePoint;
+            this.yTwoPoint = yTwoPoint;
+        }
+        public double LengthCalculate()
+        {
+           double result = Math.Sqrt(Math.Pow(this.xTwoPoint - this.xOnePoint, 2) + Math.Pow(this.yTwoPoint - this.yOnePoint, 2));
+            return result;
+        }
+    }
+}
